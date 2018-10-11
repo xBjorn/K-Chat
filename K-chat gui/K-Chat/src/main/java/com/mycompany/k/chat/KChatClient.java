0a6700c016c9099277e1a6d146588b60c58fd10c
Yukming <yukming1996@gmail.com>
2018-10-11 02:09:47 +0200

    klmk

--- K-chat gui/K-Chat/src/main/java/com/mycompany/k/chat/KChatClient.java
+++ K-chat gui/K-Chat/src/main/java/com/mycompany/k/chat/KChatClient.java
@@ -4,36 +4,35 @@ import java.net.*;
 import java.io.*;
 
 public class KChatClient extends Thread {
-        private InputStream input;
+
+	private InputStream input;
 	private Socket clientSocket;
-        private PrintWriter out;
+	private PrintWriter out;
 	private String ip;
 	private int port;
-        private String msg;
+	private String msg;
 
 	public KChatClient(String ip, int port, String msg) {
 		this.ip = ip;
 		this.port = port;
-                this.msg = msg;
-		
+		this.msg = msg;
+
 	}
-        
-	public void run(String ip, int port, String msg) {
+
+	public void run() {
 		try {
-                    System.out.println("run");
+			System.out.println("run");
+			System.out.println(this.msg);
 			clientSocket = new Socket(this.ip, this.port);
 			System.out.println("connecting with: " + getIp());
-                        out = new PrintWriter(clientSocket.getOutputStream());
-                        out.write(msg);
-                        out.flush();
-                        
-                        
+			out = new PrintWriter(clientSocket.getOutputStream());
+			out.write(this.msg);
+			out.flush();
+
 //                        input = clientSocket.getInputStream();
 //                        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 //                        String line = reader.readLine();
 //                        System.out.println(line);
-                 
-                      
 		} catch (IOException sweetError) {
 			System.out.println("failed to create clientsocket?: " + sweetError);
 		}
@@ -46,7 +45,6 @@ public class KChatClient extends Thread {
 	public int getPort() {
 		return port;
 	}
-
 
 	public void stopConnection() {
 		try {
